15d17b34408772cc2cef110bd2a38d5e9d5d47ff
Matheus Zonaro <matheuszonarobr@gmail.com>
2021-05-03 10:14:38 -0300

    First commit

---
+++ backend/src/main/java/com/zonaro/dsvendas/DsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.zonaro.dsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/zonaro/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zonaro.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
